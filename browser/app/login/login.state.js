a0dadbb929ae1933ffc60e660cfcaaba2d46064a
Joe Alves <joe@fullstackacademy.com>
2016-08-31 13:04:48 -0400

    4.Login

--- browser/app/login/login.state.js
+++ browser/app/login/login.state.js
@@ -3,6 +3,17 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('login', {
     url: '/login',
-    templateUrl: '/browser/app/login/login.html'
+    templateUrl: '/browser/app/login/login.html',
+    controller: function ($scope, AuthFactory) {
+
+      $scope.submitLogin = function (email, password) {
+        console.log(email);
+        AuthFactory.login(email, password)
+          .then(function () {
+              // TBD
+          });
+      };
+
+    }
   });
 });
